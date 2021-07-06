75ffcefa49d98205f7e8f22783b2a10026e1a1ab
pspiagicw <pspiagicw@gmail.com>
2021-07-06 17:24:53 +0530

    Uploaded to PyPI:)
    Added Sylvie Image
    
    Added PYPi in readme

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='sylvie',
+      version='1.0',
+      description='Sylvie: Enchant your Data',
+      author='pspiagicw',
+      author_email='pspiagicw@gmail.com',
+      url='https://github.com/pspiagicw/sylvie',
+      py_modules=['sylvie'],
+      scripts=['sylvie'],
+      )
