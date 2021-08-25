bd08b67a8dae73ec96daf2bbee1a3030955dde3f
pspiagicw <pspiagicw@gmail.com>
2021-08-25 07:38:41 +0530

    Added form of documentation

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-from distutils.core import setup
-
+from setuptools import setup
+long_description = ''
+with open('README.md') as longfile:
+    long_description=longfile.read()
 setup(name='sylvie',
-      version='1.0',
+      version='1.0.1',
       description='Sylvie: Enchant your Data',
       author='pspiagicw',
       author_email='pspiagicw@gmail.com',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       url='https://github.com/pspiagicw/sylvie',
       py_modules=['sylvie'],
       scripts=['sylvie'],
